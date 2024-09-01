54527eb9d2bd01a9471b01ea8c6e8bfb68202351
alex <aleksandr.nesterov@notino.com>
2024-09-01 14:15:13 +0200

    Initial Commit

---
+++ pinchAndZoom/pinchAndZoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  pinchAndZoomApp.swift
+//  pinchAndZoom
+//
+//  Created by Aleksandr Nesterov on 01.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct pinchAndZoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
